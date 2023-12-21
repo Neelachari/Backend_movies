c2dda6b4edc3f6a2e72d4411001e3547c6c5a8f7
Neelachari N H <neelachari34@gmail.com>
2023-12-21 18:49:57 +0530

    initial commit with Data

---
+++ index.js
@@ -0,0 +1,32 @@
+const express=require("express")
+const connection = require("./Config/db")
+const signupRouter=require("./Routes/Signup.route")
+const LoginRouter=require("./Routes/Login.route")
+const movie=require("./Routes/movies.route")
+const cors=require("cors")
+const app=express()
+
+app.use(express.json())
+
+app.use(cors())
+
+app.use("/users",signupRouter)
+
+app.use("/users",LoginRouter)
+
+app.use("/movies",movie)
+
+require("dotenv").config()
+
+app.get("/", (req,res)=>{
+    res.send("Welcome to Movies")
+})
+
+app.listen(process.env.PORT, async()=>{
+    try {
+       await connection
+       console.log("Server Running on Port", process.env.PORT) 
+    } catch (error) {
+        console.log(error)
+    }
+})
